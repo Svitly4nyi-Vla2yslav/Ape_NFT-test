40da95bad809dfc3e19b4b29831e0aa3bc7804ec
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-04-27 01:45:03 +0200

    add form

--- src/components/ContactUs/ContactForm.tsx
+++ src/components/ContactUs/ContactForm.tsx
@@ -1,59 +1,64 @@
-
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 import { successMessage } from './SuccessMessage';
-import { ButtonSubmit, ButtonTextContact } from './ContactUs.styled';
-import * as Yup from 'yup';
-import { withFormik, FormikProps, FormikErrors, Form, Field } from 'formik';
-import discord from "../../assets/icons/discord.svg"
-
+import {
+  ButtonSubmit,
+  ButtonTextContact,
+  ContactContainer,
+  ContactText,
+  ContainerForm,
+  ContainerIcon,
+  DiscordIcon,
+  FoxIcon,
+  InputDiscord,
+  InputFox,
+} from './ContactUs.styled';
+// import * as Yup from 'yup';
+// import { withFormik, FormikProps, FormikErrors, Form, Field } from 'formik';
+import discord from '../../assets/icons/discord.svg';
+import fox from "../../assets/icons/fox.svg"
 interface FormValues {
-    email: string;
-    name: string;
-  }
-  
- 
+  email: string;
+  name: string;
+}
 
 export const ContactForm: React.FC = () => {
-  const [state, handleSubmit] = useForm("mjvnpjav");
+  const [state, handleSubmit] = useForm('mjvnpjav');
   if (state.succeeded) {
-      successMessage();
+    successMessage();
   }
 
-
   return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-        placeholder='@username'
-        src={discord}
-        prefix={discord}
-        
-        
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <ButtonSubmit type="submit" disabled={state.submitting}>
-       <ButtonTextContact>Mint</ButtonTextContact>
-      </ButtonSubmit>
-    </form>
+    <>
+      <ContainerForm onSubmit={handleSubmit} id='mint'>
+        <label htmlFor="email"></label>
+        <ContainerIcon>
+          <DiscordIcon src={discord}  alt='💠'/>
+          <InputDiscord
+            id="name"
+            type="name"
+            name="name"
+            placeholder="@username"
+          />
+          <ValidationError prefix="name" field="name" errors={state.errors} />
+        </ContainerIcon>
+        <ContainerIcon>
+          <FoxIcon src={fox} alt='🦊'  />
+        <InputFox
+          id="adress"
+          name="adress"
+          type="name"
+          placeholder="Wallet address"
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        /></ContainerIcon>
+        <ButtonSubmit type="submit" disabled={state.submitting}>
+          <ButtonTextContact>Mint</ButtonTextContact>
+        </ButtonSubmit>
+      </ContainerForm>
+    </>
   );
-}
+};
--- src/components/ContactUs/ContactUs.styled.ts
+++ src/components/ContactUs/ContactUs.styled.ts
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 export const ContactContainer = styled.div`
-
+display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 216px;
+    margin : 0 auto;
 `;
 
 export const TitleContact = styled.h3`
@@ -11,6 +15,7 @@ font-size: 44px;
 line-height: 0.90909;
 text-transform: uppercase;
 color: #fff;
+text-align: center;
 `;
 
 export const ContactText = styled.p`
@@ -23,24 +28,38 @@ line-height: 1.1875;
 text-transform: uppercase;
 text-align: center;
 color: #fff;
+margin: 16px 0;
 `;
 
 
-export const IconX = styled.div`
-
+export const IconX = styled.img`
+width: 36px;
+height: 36px;
+margin-top: 24px;
 `;
 
 
 export const ContainerForm = styled.form`
+margin: 16px 72px;
+// width: 100%;
 
 `;
 
 export const InputDiscord = styled.input`
+border: 1px solid #1e1e1e;
+border-radius: 0 8px 8px 0;
+padding: 22px 24px;
+background: #000000;
+width: 168px;
 
 `;
 
 export const InputFox = styled.input`
-
+border: 1px solid #1e1e1e;
+border-radius: 0 8px 8px 0;
+padding: 22px 24px;
+background: #000000;
+width: 168px;
 `;
 
 export const IconInput = styled.svg`
@@ -48,7 +67,11 @@ export const IconInput = styled.svg`
 `;
 
 export const ButtonSubmit = styled.button`
-
+border-radius: 8px;
+width: 216px;
+height: 48px;
+backdrop-filter: blur(12px);
+background: #dc3b5a;
 `;
 
 export const ButtonTextContact = styled.p`
@@ -59,3 +82,33 @@ line-height: 1.1875;
 color: #fff;
 `;
 
+export const DiscordIcon = styled.img`
+backdrop-filter: blur(12px);
+background: #1e1e1e;
+border: 1px solid #1e1e1e;
+border-radius: 8px 0 0 8px;
+background: #1E1E1E;
+width: 48px;
+height: 48px;
+padding: 10px;
+
+
+`;
+
+export const FoxIcon = styled.img`
+backdrop-filter: blur(12px);
+background: #1e1e1e;
+border: 1px solid #1e1e1e;
+border-radius: 8px 0 0 8px;
+background: #1E1E1E;
+width: 48px;
+height: 48px;
+padding: 10px;
+`;
+
+export const ContainerIcon = styled.div`
+height: 48px;
+display: flex;
+margin-bottom: 16px;
+width: auto;
+`;
